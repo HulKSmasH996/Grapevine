e7bbf75757e70f48ceb3b6f3beedde5e1d6d88da
Subhojeet Sen <1421036@gmail.com>
2018-04-08 17:56:16 +0530

    Initial UI

--- app/src/main/java/com/kiit/viper/grapevine/MainActivity.java
+++ app/src/main/java/com/kiit/viper/grapevine/MainActivity.java
@@ -1,45 +1,266 @@
 package com.kiit.viper.grapevine;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import static com.kiit.viper.grapevine.LoginActivity.fUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    MenuItem prevMenuItem;
+    private ViewPager viewPager;
+    private BottomNavigationView bottomNavigationView;
+    FirebaseDatabase database;
+    DatabaseReference usersRef;
+    FavoritesFragment favoritesFragment;
+    HomeFragment homeFragment;
+    MessagesFragment messagesFragment;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
 
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        // toolbar.setNavigationIcon(R.drawable.common_google_signin_btn_icon_dark);
+        toolbar.setTitle("");
+        toolbar.setSubtitle("");
+
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        //Initializing the bottomNavigationView
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.navigation_favorites:
+                                viewPager.setCurrentItem(0);
+                                break;
+                            case R.id.navigation_home:
+                                viewPager.setCurrentItem(1);
+                                break;
+                            case R.id.navigation_messages:
+                                viewPager.setCurrentItem(2);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else {
+                    bottomNavigationView.getMenu().getItem(1).setChecked(false);
+                }
+                Log.d("page", "onPageSelected: " + position);
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+       /*  //Disable ViewPager Swipe
+       viewPager.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                return true;
+            }
+        });
+        */
+
+        setupViewPager(viewPager);
+        viewPager.setCurrentItem(1);
+
+        final ImageView user = (ImageView)findViewById(R.id.user);
+        // user.setImageResource(R.drawable.common_google_signin_btn_icon_light);
+
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+
+                .build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+
+                .defaultDisplayImageOptions(defaultOptions)
+                .build();
+        ImageLoader.getInstance().init(config);
+        final ProgressBar progressBar1 = (ProgressBar)findViewById(R.id.userload);
+        database = FirebaseDatabase.getInstance();
+        if(fUser!=null){
+            usersRef = database.getReference("users");
+            usersRef.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    final  UsersModel c = dataSnapshot.getValue(UsersModel.class);
+                    if(c.getUid().equals(fUser.getUid())){
+                        ImageLoader.getInstance().displayImage(c.getPhoto(),user, new ImageLoadingListener(){
+                            @Override
+                            public void onLoadingStarted(String imageUri, View view) {
+                                progressBar1.setVisibility(View.VISIBLE);
+                            }
+
+                            @Override
+                            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                                progressBar1.setVisibility(View.GONE);
+                            }
+
+                            @Override
+                            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                                progressBar1.setVisibility(View.GONE);
+                            }
+
+                            @Override
+                            public void onLoadingCancelled(String imageUri, View view) {
+                                progressBar1.setVisibility(View.GONE);
+                            }
+                        });
+                        user.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(getApplicationContext(), UserDetails.class);
+                                // Intent intent = new Intent(this, MyActivity.class);
+                                Bundle extras = new Bundle();
+
+                                extras.putString("EXTRA_NAME", c.getName());
+                                extras.putString("EXTRA_EMAIL",c.getEmail());
+                                extras.putString("EXTRA_PHOTO",c.getPhoto());
+                                intent.putExtras(extras);
+                                startActivity(intent);
+
+                            }
+                        });
+                    }
+
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+        else {
+            user.setImageResource(R.drawable.logo);
+            progressBar1.setVisibility(View.GONE);
+        }
+
+
+
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        homeFragment=new HomeFragment();
+        favoritesFragment=new FavoritesFragment();
+        messagesFragment = new MessagesFragment();
+        adapter.addFragment(favoritesFragment);
+        adapter.addFragment(homeFragment);
+        adapter.addFragment(messagesFragment);
+        viewPager.setAdapter(adapter);
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.logout) {
+            FirebaseAuth.getInstance().signOut();
+
+
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+           /* ClearCache.getInstance().clearApplicationData();
+            AlarmManager mgr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+            mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, PendingIntent.getActivity(this.getBaseContext(), 0, new    Intent(getIntent()), getIntent().getFlags()));
+            android.os.Process.killProcess(android.os.Process.myPid());*/
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
 }
---
+++ app/src/main/java/com/kiit/viper/grapevine/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.kiit.viper.grapevine;
+
+/**
+ * Created by VIPER on 08-Apr-18.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by VIPER on 24-Mar-18.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+}
