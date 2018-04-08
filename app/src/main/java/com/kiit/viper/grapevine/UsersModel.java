a4a3aade9866cc933aaa87a9d30707fe537c5464
Subhojeet Sen <1421036@gmail.com>
2018-04-08 16:48:41 +0530

    Initial commit

---
+++ app/src/main/java/com/kiit/viper/grapevine/UsersModel.java
@@ -0,0 +1,63 @@
+package com.kiit.viper.grapevine;
+
+/**
+ * Created by VIPER on 08-Apr-18.
+ */
+
+public class UsersModel {
+
+    String name;
+    String photo;
+    String email;
+    String uid;
+    String questions;
+    String answers;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(String questions) {
+        this.questions = questions;
+    }
+
+    public String getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(String answers) {
+        this.answers = answers;
+    }
+}
